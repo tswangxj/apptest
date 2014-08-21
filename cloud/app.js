ed8272719f2fb8ef250d2aa2da22d97403815c30
tswangxj <tswangxj@gmail.com>
2014-08-21 23:17:55 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -1,68 +1,16 @@
-// 在Cloud code里初始化express框架
+// 在 Cloud code 里初始化 Express 框架
 var express = require('express');
 var app = express();
-var name = require('cloud/name.js');
-var avosExpressHttpsRedirect = require('avos-express-https-redirect');
 
+// App 全局配置
+app.set('views','cloud/views');   // 设置模板目录
+app.set('view engine', 'ejs');    // 设置 template 引擎
+app.use(express.bodyParser());    // 读取请求 body 的中间件
 
-// App全局配置
-//设置模板目录
-if(__production)
-	app.set('views', 'cloud/views');
-else
-	app.set('views', 'cloud/dev_views');
-app.set('view engine', 'ejs');    // 设置template引擎
-app.use(avosExpressHttpsRedirect()); //启用HTTPS
-app.use(express.bodyParser());    // 读取请求body的中间件
-
-//使用express路由API服务/hello的http GET请求
+// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
 app.get('/hello', function(req, res) {
-	res.render('hello', { message: 'Congrats, you just set up your app!' });
+  res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-var Visitor = AV.Object.extend('Visitor');
-function renderIndex(res, name){
-	var query = new AV.Query(Visitor);
-	query.skip(0);
-	query.limit(10);
-	query.descending('createdAt');
-	query.find({
-		success: function(results){
-			res.render('index',{ name: name, visitors: results});
-		},
-		error: function(error){
-			console.log(error);
-			res.render('500',500)
-		}
-	});
-
-}
-
-app.get('/', function(req, res){
-	var name = req.query.name;
-	if(!name)
-		name = 'AVOS Cloud';
-	renderIndex(res, name);
-});
-
-app.post('/',function(req, res){
-	var name = req.body.name;
-	if(name && name.trim() !=''){
-		//Save visitor
-		var visitor = new Visitor();
-		visitor.set('name', name);
-		visitor.save(null, {
-			success: function(gameScore) {
-				res.redirect('/?name=' + name);
-			},
-			error: function(gameScore, error) {
-				res.render('500', 500);
-			}
-		});
-	}else{
-		res.redirect('/');
-	}
-});
-
-// This line is required to make Express respond to http requests.
+// 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
--- cloud/name.js
+++
@@ -1,4 +0,0 @@
-var coolNames = ['Ralph', 'Skippy', 'Chip', 'Ned', 'Scooter'];
-exports.isACoolName = function(name) {
-  return coolNames.indexOf(name) !== -1;
-}
