c7bf1495b76160dbdb0cd57d62faaa0753e85106
tswangxj <tswangxj@gmail.com>
2014-08-21 23:07:45 +0800

    tttt

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,36 @@
-require("cloud/app.js");
-// Use AV.Cloud.define to define as many cloud functions as you want.
+// Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+var name = require('cloud/name.js');
+require('cloud/app.js')
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    console.log(request.user);
+	response.success("Hello world," + request.params.name);
+});
+
+AV.Cloud.beforeSave("TestReview", function(request, response){
+	if (request.object.get("stars") < 1) {
+		response.error("you cannot give less than one star");
+	} else if (request.object.get("stars") > 5) {
+		response.error("you cannot give more than five stars");
+	} else {
+		var comment = request.object.get("comment");
+		if (comment && comment.length > 140) {
+			// Truncate and add a ...
+			request.object.set("comment", comment.substring(0, 137) + "...");
+		}
+		response.success();
+	}
+});
+
+AV.Cloud.afterSave("TestReview", function(request) {
+	var query = new AV.Query("TestPost");
+	query.get(request.object.get("post").id, {
+		success: function(post) {
+			post.increment("comments");
+			post.save();
+		},
+		error: function(error) {
+			throw "Got an error " + error.code + " : " + error.message;
+		}
+	});
 });
